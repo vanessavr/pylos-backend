f788d038c410f73bce2197b0cd3b3bb87d09deb5
vanessavr <110788192+vanessavr@users.noreply.github.com>
2024-03-27 16:51:27 -0500

    Test

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { RespuestaPruebaDiagnosticaModule } from './respuesta-prueba-diagnostica
 import { AuthModule } from './auth/auth.module'
 import { PrismaModule } from './prisma/prisma.module'
 
+// Test
 @Module({
     imports: [UsuarioModule, PreguntaSeguridadModule, PreguntaPruebaDiagnosticaModule, OpcionPruebaDiagnosticaModule, RespuestaPruebaDiagnosticaModule, AuthModule, PrismaModule],
     controllers: [AppController],
