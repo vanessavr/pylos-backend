8105db2e2e119f2ae41c205535c2df00a0a3cb65
Cristian <carincon93@gmail.com>
2024-03-29 15:27:34 -0500

    Cookie domain added

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,27 +1,42 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
+import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common'
+import { CanActivate } from '@nestjs/common/interfaces/features/can-activate.interface'
+import { Request, Response } from 'express' // Ensure correct import from Express
 import { JwtService } from '@nestjs/jwt'
-import { Request } from 'express'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private jwtService: JwtService) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        const request = context.switchToHttp().getRequest()
+        const request = context.switchToHttp().getRequest<Request>()
+        const response = context.switchToHttp().getResponse<Response>()
         const token = this.extractTokenFromHeader(request)
+
         if (!token) {
             throw new UnauthorizedException()
         }
+
         try {
             const payload = await this.jwtService.verifyAsync(token, {
                 secret: process.env.JWT_SECRET,
             })
+
+            // Set cookie with correct frontend domain
+            const cookieOptions = {
+                domain: process.env.NEXTJS_PUBLIC_URL, // Use the correct frontend domain here
+                httpOnly: true,
+                // Other cookie options like maxAge, secure, etc.
+            }
+
+            response.cookie('accessToken', token, cookieOptions)
+
             // 💡 We're assigning the payload to the request object here
             // so that we can access it in our route handlers
             request['user'] = payload
         } catch {
             throw new UnauthorizedException()
         }
+
         return true
     }
 
