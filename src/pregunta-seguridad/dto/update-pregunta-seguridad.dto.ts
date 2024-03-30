dbb585d85282e3120b9e355703c13f3ea0f34304
Cristian <carincon93@gmail.com>
2024-03-30 10:57:13 -0500

    pregunta seguridad deleted

--- src/pregunta-seguridad/dto/update-pregunta-seguridad.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger'
-import { CreatePreguntaSeguridadDto } from './create-pregunta-seguridad.dto'
-
-export class UpdatePreguntaSeguridadDto extends PartialType(CreatePreguntaSeguridadDto) {}
